5d29d20818a5d211b45e24dfb93163cc1b5d3980
isak3233 <isak.heimdahl@gmail.com>
2025-10-13 13:48:07 +0200

    test

--- Program.cs
+++ Program.cs
@@ -4,18 +4,7 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Stad!");
-
-            Console.WriteLine("hej /anton");
-
-            Console.Write("Helo");
-
-            Console.Write("\ntest");
-            Console.Write("Nytt test");
-=======
             Simulation simulation = new Simulation();
->>>>>>> c41fede (La till klasser och la till funktionen att skriva ut staden med personer i)
         }
     }
 }
